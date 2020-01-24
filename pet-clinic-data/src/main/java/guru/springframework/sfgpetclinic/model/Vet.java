3ec1a279ae9d67672f1be581120168ee2c7b331b
Mirna Cantero <mirnacantero@gmail.com>
2020-01-24 10:24:51 -0300

    Added Specialty, associated to Vet. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Specialty> especialties;
+
+    public Set<Specialty> getEspecialties() {
+        return especialties;
+    }
+
+    public void setEspecialties(Set<Specialty> especialties) {
+        this.especialties = especialties;
+    }
 }
